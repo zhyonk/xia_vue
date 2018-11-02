4240983e0d497315a87d347fa164c4c10eaf6771
zhyonk <zhyonk@163.com>
2018-11-02 22:48:42 +0800

    会员卡页面

--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -1,5 +1,15 @@
 <template>
+<div>
   <x-header>我的会员卡</x-header>
+  <div class="am-page-empty class-fullcenter am-clickable" style="display: block;">
+    <div>
+    <img src="../assets/images/card_space.png">
+    <p>您目前没有任何
+    <em class="v4_myCard">会员卡</em>，轻触屏幕可查看
+    <em class="v4_myCard">会员卡</em>类型</p>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -22,3 +32,24 @@ export default {
   }
 }
 </script>
+
+<style>
+.panel_container.memberCards .am-page-empty p {
+    font-size: 12px;
+    line-height: 20px;
+}
+.am-page-empty p {
+    position: absolute;
+    line-height: 50px;
+    top: 45%;
+    text-align: center;
+    color: #aaa;
+}
+.class-fullcenter img{
+    position: absolute;
+    height: 150px;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
