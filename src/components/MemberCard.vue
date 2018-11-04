4e724fdeffa051232ff7e7acebd296dabce7102b
zhyonk <zhyonk@163.com>
2018-11-04 22:05:13 +0800

    修改会员卡页面样式，添加优惠卷功能

--- src/components/MemberCard.vue
+++ src/components/MemberCard.vue
@@ -6,67 +6,108 @@
       <div>
       <!-- <img class="background" src="../assets/images/card_space.png"> -->
       <p>您目前没有任何
-      <em class="v4_myCard">会员卡</em>，轻触屏幕可查看
-      <em class="v4_myCard">会员卡</em>类型</p>
+      <em v-show="isShow" class="v4_myCard">会员卡</em>，轻触屏幕可查看
+      <em v-show="isShow" class="v4_myCard">会员卡</em>类型</p>
       </div>
     </div>
   </div>
   <div class="cardList">
-    <li class ="card">
-      <div class="content">
-      <!-- <img src="../assets/images/hua.png" /> -->
-      <p class="center">
-        <img src="http://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eqNeOZ9Nibv3jTRXZsvtlsILiazVXhGLicLa6emMCgx9X2DY0rjqQmicOGD3ly0p1LIedyfoXGYcPPSqA/132">
-      </p>
-      <h1 class="cardTypeName">我的会员卡</h1>
+  <group>
+    <cell title="卡包" is-link :value="cardCount" link="/mcardList">
+        <img slot="icon" width="20" style="display:block;margin-right:5px;" src="../assets/images/card.png">
+    </cell>
+    
+  <van-loading v-show="loadCardList" style="margin:auto"/>
+    <div class ="card" v-for="item in cardListInfo" :key="item.memberCardNumber" @click="item.detail" :style="{ 'background-color': item.memberCardTypeColor }">
+      <div class="content"  >
+      <div class="center">
+        <h1 class="cardTypeName">{{item.memberCardTypeName}}</h1>
+        <img class="headImage" src="http://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eqNeOZ9Nibv3jTRXZsvtlsILiazVXhGLicLa6emMCgx9X2DY0rjqQmicOGD3ly0p1LIedyfoXGYcPPSqA/132">
+        <h1 class="discount">{{item.discounts}}折</h1>
+        <h1 class="remainder">余额 ：{{item.remainderPrice}}</h1>
+        <h1 class="deadline">过期时间：{{item.deadline}}</h1>
       </div>
-    </li>
+      </div>
+    </div>
+    <br/>
+  </group>
+
+  <group>
+    <cell title="优惠卷" is-link  style="height:50px" link="/coupon">
+        <img slot="icon" width="20" style="display:block;margin-right:5px;" src="../assets/images/coupon.png">
+    </cell>
+  </group>
+    
   </div>
 </div>
-
 </template>
 
 <script>
-// import net from '../utils/net'
 import axios from '../axios/https.js'
-import { XHeader } from 'vux'
-// import auth from '../aouth/auth'
+import { XHeader, CellBox, Cell, Group } from 'vux'
 import store from '@/store/store'
 import * as types from '@/store/types'
-// import { Button, Cell } from 'vant'
+import { Loading } from 'vant'
+import Vue from 'vue'
+import utils from '../utils/utils.js'
+import router from '../router/router.js'
+Vue.use(Loading)
 
 export default {
   components: {
-    XHeader
+    XHeader,
+    CellBox,
+    Cell,
+    Group
   },
   data () {
     return {
       error: '',
-      isShow: true
+      isShow: false,
+      loadCardList: true,
+      cardListInfo: [],
+      cardCount: '查看全部(0)'
     }
   },
   mounted: function () {
     // 设置
-    this.getMemberCard()
-    var memberCardInfo = store.getters.memberCardInfo
-    console.log('Info: ' + memberCardInfo)
-    if (memberCardInfo == null) {
-      console.log('memberCardInfo: not show')
-      this.isShow = false
-    } else {
-      console.log('memberCardInfo: show')
-      this.isShow = true
+    var info = this.getMemberCard()
+    this.cardListInfo = info
+    if (info != null && info !== '') {
+      this.loadCardList = false
+      this.cardCount = '查看全部(' + info.length + ')'
     }
   },
   methods: {
     getMemberCard () {
+      var _this = this
       axios.get('/user/getMemberCard', {params: {openId: sessionStorage.getItem('openid')}}, {headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }}).then(function (response) {
         var memberCardInfo = response.data.data.memberCardInfo
-        console.log(memberCardInfo)
-        if (memberCardInfo != null && memberCardInfo !== '' && memberCardInfo.length !== 0) {
+        console.log('memberCardInfo:' + memberCardInfo.length)
+        if (memberCardInfo != null && memberCardInfo !== '') {
           store.commit(types.MEMBERCARDINFO, memberCardInfo)
+          for (var i in memberCardInfo) {
+            memberCardInfo[i].deadline = utils.formatDate(new Date(memberCardInfo[i].deadline))
+            memberCardInfo[i].detail = function () {
+              router.push('/mcardDetail/' + memberCardInfo[i].memberCardNumber)
+            }
+          }
+          _this.cardListInfo = memberCardInfo
+
+          if (memberCardInfo != null && memberCardInfo !== '') {
+            _this.loadCardList = false
+            _this.cardCount = '查看全部(' + memberCardInfo.length + ')'
+          }
+          console.log('Info1: ' + memberCardInfo)
+          if (memberCardInfo) {
+            console.log('memberCardInfo: not show')
+            _this.isShow = false
+          } else {
+            console.log('memberCardInfo: show')
+            _this.isShow = true
+          }
           return memberCardInfo
         }
       }).catch(function (error) {
@@ -101,7 +142,6 @@ export default {
   width: 100%;
 }
 .card{
-  background-color:red;
   list-style:none;
   margin-top: 8px;
   margin-left: 8px;
@@ -111,6 +151,7 @@ export default {
   -webkit-box-shadow: 3px 3px 6px #666; 
   -moz-box-shadow: 3px 3px 6px #666; 
   box-shadow: 3px 3px 6px #666; 
+  margin-bottom: 10px
 }
 .card .background{
   position: absolute;
@@ -134,7 +175,7 @@ export default {
   font-size: 18px;
   height: 100%;
 }
-.center img {
+.headImage {
   position: absolute;
   width: 60px;
   height: 60px;
@@ -145,17 +186,56 @@ export default {
 }
 .cardTypeName {
    position: absolute;
-    margin: 0 88px;
-    height: 40px;
     width: auto;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    line-height: 40px;
     text-align: center;
     font-size: 18px;
     font-weight: 400;
     color: #fff;
+    margin:auto;
+    display: block;
+    left:70%;
+    margin-left:-125px;
+    top: 10%;
+}
+.discount{
+   position: absolute;
+    width: auto;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 400;
+    color: #fff;
+    margin:auto;
+    display: block;
+    left:70%;
+    margin-left:25px;
+    top: 30%;
+}
+.remainder{
+   position: absolute;
+    width: auto;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 400;
+    color: #fff;
+    margin:auto;
+    display: block;
+    left:70%;
+    margin-left:-125px;
+    top: 50%;
+}
+.deadline{
+   position: absolute;
+    width: auto;
+    text-align: center;
+    font-size: 10px;
+    font-weight: 400;
+    color: #c55d5d;
+    margin:auto;
+    display: block;
+    left:50%;
+    margin-left:70px;
+    top: 70%;
+    font-style: italic
 }
 
 </style>
